8f0c22f9edbe285cdc9b95f4de21720e68b234a6
youssef-erremili <yousseferremili19@gmail.com>
2024-09-02 19:30:28 +0100

    Create the model

--- assets/main.js
+++ assets/main.js
@@ -1,1 +1,23 @@
-console.log(true);
+// const category = 'computers';
+// const url = 'https://api.api-ninjas.com/v1/quotes?category=' + category;
+// const apiKey = 'yfixoj9cWc3s7UmBI2lGfQ==2QNByH09NUJv47SR';
+
+// fetch(url, {
+//     method: 'GET',
+//     headers: {
+//         'X-Api-Key': apiKey,
+//         'Content-Type': 'application/json'
+//     }
+// })
+//     .then(response => {
+//         if (!response.ok) {
+//             throw new Error('Network response was not ok ' + response.statusText);
+//         }
+//         return response.json();
+//     })
+//     .then(data => {
+//         console.log(data);
+//     })
+//     .catch(error => {
+//         console.error('There was a problem with the fetch operation:', error);
+//     });
